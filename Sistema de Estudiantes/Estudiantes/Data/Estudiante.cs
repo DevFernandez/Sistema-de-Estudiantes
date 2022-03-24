cc4152eff9b34230ecfd43d0ab6f4816995a3b26
Cristian <dev28@gmail.com>
2022-03-24 19:47:10 -0300

    Version 1.0

---
+++ Sistema de Estudiantes/Estudiantes/Data/Estudiante.cs
@@ -0,0 +1,20 @@
+﻿using LinqToDB.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class Estudiante
+    {
+        [PrimaryKey, Identity]
+        public int ID { set; get; }
+        public string NID { set; get; }
+        public string Nombre { set; get; }
+        public string Apellido { set; get; }
+        public string Email { set; get; }
+        public byte[] foto { set; get; }
+    }
+}
